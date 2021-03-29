99539c184b68465ff67e513f3f494544202d45e5
rachdev09 <rachdev09@gmail.com>
2021-03-29 02:14:33 -0400

    Style fixes

--- src/app/mortgage-calculator/mortgage-calculator.module.ts
+++ src/app/mortgage-calculator/mortgage-calculator.module.ts
@@ -5,7 +5,6 @@ import {MortgageCalculatorPageComponent} from '@mortgage-calculator/mortgage-cal
 import {PaymentPlanComponent} from '@mortgage-calculator/components/payment-plan/payment-plan.component';
 import {CalculationSummaryComponent} from '@mortgage-calculator/components/calculation-summary/calculation-summary.component';
 import {MortgageSummaryComponent} from '@mortgage-calculator/components/mortgage-summary/mortgage-summary.component';
-import {PaymentDiagramComponent} from '@mortgage-calculator/components/payment-diagram/payment-diagram.component';
 import {MortgageCalculatorRoutingModule} from '@mortgage-calculator/mortgage-calculator-routing.module';
 import {PaymentScheduleComponent} from '@mortgage-calculator/components/payment-schedule/payment-schedule.component';
 import {MortgageService} from '@mortgage-calculator/mortgage.service';
@@ -17,7 +16,6 @@ import {CommonModule} from '@angular/common';
     PaymentPlanComponent,
     CalculationSummaryComponent,
     MortgageSummaryComponent,
-    PaymentDiagramComponent,
     PaymentScheduleComponent
   ],
   imports: [
